8815139dd9deb7799ce76dba8614a731c4b77185
Oscar Andersson <anderssonoscar03@gmail.com>
2025-11-24 22:50:59 +0100

    adjust opacity for messaging windows

--- modules/home/hyprland/windowrules.nix
+++ modules/home/hyprland/windowrules.nix
@@ -65,7 +65,7 @@ in {
         "size 60% 70%, class:^([Ff]erdium)$"
         "opacity 1.0 1.0, tag:browser*"
         "opacity 0.9 0.8, tag:projects*"
-        "opacity 0.94 0.86, tag:im*"
+        "opacity 1.0 1.0, tag:im*"
         "opacity 0.9 0.8, tag:file-manager*"
         "opacity 0.8 0.7, tag:terminal*"
         "opacity 0.8 0.7, tag:settings*"
