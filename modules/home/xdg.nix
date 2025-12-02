bbab3d3cfc11ec474bcc4acdb06d80c2539bb55d
Oscar Andersson <anderssonoscar03@gmail.com>
2025-12-02 23:15:56 +0100

    set default browser

--- modules/home/xdg.nix
+++ modules/home/xdg.nix
@@ -4,6 +4,14 @@
     mime.enable = true;
     mimeApps = {
       enable = true;
+      defaultApplications = {
+        "text/html" = "firefox.desktop";
+        "application/xhtml+xml" = "firefox.desktop";
+        "x-scheme-handler/http" = "firefox.desktop";
+        "x-scheme-handler/https" = "firefox.desktop";
+        "x-scheme-handler/about" = "firefox.desktop";
+        "x-scheme-handler/unknown" = "firefox.desktop";
+      };
     };
     portal = {
       enable = true;
