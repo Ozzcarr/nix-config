10389e84270486940156a6d2b2f0894e45a5dbe0
Oscar Andersson <anderssonoscar03@gmail.com>
2025-12-14 21:10:05 +0100

    add udev rules for keymapp flashing

--- modules/core/hardware.nix
+++ modules/core/hardware.nix
@@ -15,4 +15,8 @@
     bluetooth.powerOnBoot = true;
   };
   local.hardware-clock.enable = false;
+  services.udev.extraRules = ''
+    # Keymapp Flashing rules for the Voyager
+    SUBSYSTEMS=="usb", ATTRS{idVendor}=="3297", MODE:="0666", SYMLINK+="ignition_dfu"
+  '';
 }
