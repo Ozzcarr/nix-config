01f0be467ea7ce70a98b61379dc32bf4f9d69580
Oscar Andersson <anderssonoscar03@gmail.com>
2026-02-14 00:49:31 +0100

    set firefox as default for pdfs

--- modules/home/xdg.nix
+++ modules/home/xdg.nix
@@ -7,6 +7,7 @@
       defaultApplications = {
         "text/html" = "firefox.desktop";
         "application/xhtml+xml" = "firefox.desktop";
+        "application/pdf" = "firefox.desktop";
         "x-scheme-handler/http" = "firefox.desktop";
         "x-scheme-handler/https" = "firefox.desktop";
         "x-scheme-handler/about" = "firefox.desktop";
