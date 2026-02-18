cf4a34c722be4bdf8d290198a7688d69e65f7b52
Oscar Andersson <anderssonoscar03@gmail.com>
2026-02-18 23:59:19 +0100

    clean python shell

--- modules/home/scripts/shells/templates/python-shell.nix
+++ modules/home/scripts/shells/templates/python-shell.nix
@@ -2,10 +2,6 @@ with import <nixpkgs> {};
 pkgs.mkShell {
   name = "__PROJECT_NAME__";
 
-  # Here is where you will add all the libraries required by your native modules
-  # You can use the following one-liner to find out which ones you need.
-  # Just make sure you have `gcc` installed.
-  # `find .venv/ -type f -name "*.so" | xargs ldd | grep "not found" | sort | uniq`
   NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [
     stdenv.cc.cc # libstdc++
     # zlib # libz (for numpy)
