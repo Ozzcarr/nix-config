49f4bc98a3d946c2996eb39b3685422d300d8ef0
Oscar Andersson <anderssonoscar03@gmail.com>
2026-04-10 19:20:24 +0200

    fix prompt
    Signed-off-by: Oscar Andersson <anderssonoscar03@gmail.com>

--- modules/home/zsh.nix
+++ modules/home/zsh.nix
@@ -18,18 +18,7 @@
       size = 10000;
     };
 
-    plugins = [
-      {
-        name = "zsh-nix-shell";
-        file = "nix-shell.plugin.zsh";
-        src = pkgs.fetchFromGitHub {
-          owner = "chisui";
-          repo = "zsh-nix-shell";
-          rev = "v0.8.0";
-          sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
-        };
-      }
-    ];
+    plugins = [];
 
     initContent = ''
       bindkey "\eh" backward-word
@@ -88,6 +77,7 @@
           Artix = "󰣇";
           CentOS = "";
           Debian = "󰣚";
+          NixOS = "";
           Redhat = "󱄛";
           RedHatEnterprise = "󱄛";
         };
