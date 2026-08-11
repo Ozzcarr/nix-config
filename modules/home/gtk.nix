e272a7fcc573b8d1a4d240a6282ecb04813965fb
Oscar Andersson <anderssonoscar03@gmail.com>
2026-08-11 11:58:58 +0200

    restore catppuccin GTK theme, dropped when stylix was removed

--- modules/home/gtk.nix
+++ modules/home/gtk.nix
@@ -1,5 +1,11 @@
 { pkgs, lib, ... }:
-
+let
+  catppuccinGtk = pkgs.catppuccin-gtk.override {
+    accents = [ "mauve" ];
+    variant = "mocha";
+  };
+  themeName = "catppuccin-mocha-mauve-standard";
+in
 {
   dconf.settings = {
     "org/gnome/desktop/interface" = {
@@ -9,15 +15,30 @@
   };
 
   gtk = {
+    # Icon theme and extraConfig apply without this, but the theme package
+    # below is only actually installed (home.packages) when this is true.
+    enable = true;
     iconTheme = {
       name = "Papirus-Dark";
       package = pkgs.papirus-icon-theme;
     };
+    # Symbolic icons (tray icons among them) render in whatever color this
+    # theme's CSS gives them, which is how they end up Catppuccin-tinted.
+    theme = {
+      name = themeName;
+      package = catppuccinGtk;
+    };
     gtk3.extraConfig = {
       gtk-application-prefer-dark-theme = 1;
     };
-    gtk4.extraConfig = {
-      gtk-application-prefer-dark-theme = 1;
+    gtk4 = {
+      extraConfig = {
+        gtk-application-prefer-dark-theme = 1;
+      };
+      theme = {
+        name = themeName;
+        package = catppuccinGtk;
+      };
     };
   };
 }
