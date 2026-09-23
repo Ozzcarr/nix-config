297039b44e900028f1cf17409c72780e9f0d5c7e
Oscar Andersson <anderssonoscar03@gmail.com>
2026-09-23 03:01:21 +0200

    change display scaling and sensitivity on remote connect

--- hosts/desktop/sunshine.nix
+++ hosts/desktop/sunshine.nix
@@ -1,3 +1,29 @@
+{ pkgs, ... }:
+let
+  streamingProfile = "Desktop Streaming";
+  streamingSensitivity = "-1";
+
+  sunshineStreamingWatch = pkgs.writeShellScript "sunshine-streaming-watch" ''
+    hyprmoncfg=${pkgs.unstable.hyprmoncfg}/bin/hyprmoncfg
+    hyprctl=${pkgs.hyprland}/bin/hyprctl
+    journalctl=${pkgs.systemd}/bin/journalctl
+    systemctl=${pkgs.systemd}/bin/systemctl
+
+    "$journalctl" --user -u sunshine -f -n0 -o cat | while IFS= read -r line; do
+      case "$line" in
+        *"CLIENT CONNECTED"*)
+          "$systemctl" --user stop hyprmoncfgd
+          "$hyprmoncfg" apply "${streamingProfile}" --confirm-timeout 0
+          "$hyprctl" keyword input:sensitivity "${streamingSensitivity}"
+          ;;
+        *"CLIENT DISCONNECTED"*)
+          "$systemctl" --user start hyprmoncfgd
+          "$hyprctl" reload
+          ;;
+      esac
+    done
+  '';
+in
 {
   services.sunshine = {
     enable = true;
@@ -6,6 +32,22 @@
     capSysAdmin = true;
     settings = {
       sunshine_name = "desktop";
+    };
+  };
+
+  systemd.user.services.sunshine-streaming-watch = {
+    description = "Switches to the streaming hyprmoncfg profile while a Moonlight client is connected";
+    after = [
+      "graphical-session.target"
+      "sunshine.service"
+    ];
+    partOf = [ "graphical-session.target" ];
+    wantedBy = [ "graphical-session.target" ];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${sunshineStreamingWatch}";
+      Restart = "on-failure";
+      RestartSec = 5;
     };
   };
 }
